285ed0f5ff755ca8879606968a2da0af215a8f28
_sevenCao <406238940@qq.com>
2017-07-06 17:52:28 +0800

    第一次提交代码

---
+++ FirstProject/FirstProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstProject
+//
+//  Created by luhongyu on 17/7/6.
+//  Copyright © 2017年 wildLulu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstProject/FirstProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstProject
+//
+//  Created by luhongyu on 17/7/6.
+//  Copyright © 2017年 wildLulu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
